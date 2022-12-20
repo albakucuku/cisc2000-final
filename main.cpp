de6a62c8a6603f4efa701494c0a1e10f91d81535
albakucuku <114107554+albakucuku@users.noreply.github.com>
2022-12-20 17:29:20 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "timer_list.h"
+
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    TimerList timers;
+    ifstream infile("test_data.txt");
+    Timer timer;
+    while (infile >> timer)
+        timers.add(timer);
+    infile.close();
+
+    for (int i = 0; i < timers.get_size(); i++)
+        cout << timers[i] << endl;
+
+    return 0;
+}
