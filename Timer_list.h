f8ce57d458c4f0b54d6670b5f84555c277a703b2
Yohannan12 <120061659+Yohannan12@users.noreply.github.com>
2022-12-21 23:26:30 -0500

    Update Timer_list.h

--- Timer_list.h
+++ Timer_list.h
@@ -1,24 +1,33 @@
 #ifndef TIMER_LIST_H
 #define TIMER_LIST_H
-
+//header files
 #include "timer.h"
-
+//class TimerList
 class TimerList
 {
   private:
+  //variables size and capacity
+  //pointer to a pointer 
     Timer** list;
     int size;
     int capacity;
 
   public:
+  //declaration of default constructor
+  //declaration of constructir with @param capacity
     TimerList();
     TimerList(int capacity);
+  //declaration of destructor
     ~TimerList();
+  //decarations for functions add,and remove
     void add(const Timer& timer);
     void remove(int index);
+  //clear function
     void clear();
+  //constant function to get size and capacity
     int get_size() const;
     int get_capacity() const;
+  //declare overloading[] operator
     Timer& operator[](int index);
     const Timer& operator[](int index) const;
 };
