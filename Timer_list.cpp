c55107538c1b05eb397556f0c20de474baf5a1a1
Yohannan12 <120061659+Yohannan12@users.noreply.github.com>
2022-12-21 22:57:25 -0500

    Update Timer_list.cpp

--- Timer_list.cpp
+++ Timer_list.cpp
@@ -1,31 +1,32 @@
 #include "timer_list.h"
-
+//header
 #include <iostream>
 
 using namespace std;
-
+//define a constructor
 TimerList::TimerList()
 {
     list = new Timer*[10];
     size = 0;
     capacity = 10;
 }
-
+//define a constructor with capacity
 TimerList::TimerList(int capacity)
 {
     list = new Timer*[capacity];
     size = 0;
     this->capacity = capacity;
 }
-
+//detructor that deletes the list of array
 TimerList::~TimerList()
 {
     clear();
     delete[] list;
 }
-
+//function add has parameter timer
 void TimerList::add(const Timer& timer)
 {
+    //checks if size equals capacity
     if (size == capacity)
     {
         capacity *= 2;
@@ -38,7 +39,7 @@ void TimerList::add(const Timer& timer)
     list[size] = new Timer(timer);
     size++;
 }
-
+//removes list and replace it by decrementing it
 void TimerList::remove(int index)
 {
     delete list[index];
@@ -46,29 +47,29 @@ void TimerList::remove(int index)
         list[i] = list[i + 1];
     size--;
 }
-
+//clears list and sets size to 0
 void TimerList::clear()
 {
     for (int i = 0; i < size;i++)
         delete list[i];
     size = 0;
 }
-
+//get_size returns size
 int TimerList::get_size() const
 {
     return size;
 }
-
+//get_capacity returns capacity
 int TimerList::get_capacity() const
 {
     return capacity;
 }
-
+//Implements operator [] that returns list index
 Timer& TimerList::operator[](int index)
 {
     return *list[index];
 }
-
+//Implements a constant operator [] that returns list index
 const Timer& TimerList::operator[](int index) const
 {
     return *list[index];
